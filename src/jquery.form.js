5e4dbb31242a198dc5320a88512ae0dfd122402a
lefoy <me@lefoy.net>
2016-04-05 11:22:30 -0400

    Update custom global classes

--- src/jquery.form.js
+++ src/jquery.form.js
@@ -19,7 +19,7 @@
             clicked: 'is-clicked',
             extern: 'is-external',
             error: 'is-error'
-        }, this.config.customGlobalClasses || {});
+        }, (window.classes !== undefined ? window.classes : this.config.customGlobalClasses || {});
 
         this.errors = [];
         this.init();
